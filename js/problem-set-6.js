a98341473c7da4f6e917c5b009bfb3e6bae6ce16
Ironcrabman <43757458+Ironcrabman@users.noreply.github.com>
2018-12-17 16:49:35 -0500

    Update problem-set-6.js

--- js/problem-set-6.js
+++ js/problem-set-6.js
@@ -408,22 +408,22 @@ ctx.fillRect(462, 545, 100, 200);
 ctx.stroke();
 
 ctx.rect(250, 545, 100, 100)
-ctx.fillStyle = '#8ED6FF'
+ctx.fillStyle = "#8ED6FF"
 ctx.fillRect();
 ctx.stroke();
 
 ctx.rect(250, 520, 100, 100)
-ctx.fillStyle = '#8ED6FF'
+ctx.fillStyle = "#8ED6FF"
 ctx.fillRect();
 ctx.stroke();
 
 ctx.rect(545, 545, 100, 100)    
-ctx.fillStyle = '#8ED6FF'
+ctx.fillStyle = "#8ED6FF"
 ctx.fillRect();
 ctx.stroke();
 
 ctx.rect(545, 520, 100, 100)
-ctx.fillStyle = '#8ED6FF'
+ctx.fillStyle = "#8ED6FF"
 ctx.fillRect();
 ctx.stroke();
 
